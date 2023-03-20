256f23c73b075ca7d4591648e9202a601d68ef98
desperado1802 <mumun_ali@hotmail.com>
2023-03-20 21:01:08 +0200

    created the sidebar component

---
+++ components/Sidebar.jsx
@@ -0,0 +1,63 @@
+import Image from "next/image";
+import React from "react";
+import SidebarMenuItem from "./SidebarMenuItem";
+import { HomeIcon } from "@heroicons/react/solid";
+import {
+  HashtagIcon,
+  BellIcon,
+  InboxIcon,
+  BookmarkIcon,
+  ClipboardIcon,
+  UserIcon,
+  DotsCircleHorizontalIcon,
+  DotsHorizontalIcon,
+} from "@heroicons/react/outline";
+
+export default function Sidebar() {
+  return (
+    <div className="hidden sm:flex flex-col p-2 xl:items-start fixed h-full">
+      {/* Twitter Logo */}
+      <div className="hoverEffect p-0 hover:bg-blue-100 xl:py-1 ">
+        <Image
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Twitter-logo.svg/1200px-Twitter-logo.svg.png"
+          width="50"
+          height="50"
+          alt="Twitter"
+          className="m-1"
+        ></Image>
+      </div>
+
+      {/* Menu */}
+      <div className="mt-4 mb-2.5 xl:items-start">
+        <SidebarMenuItem text="Home" Icon={HomeIcon} active />
+        <SidebarMenuItem text="Explore" Icon={HashtagIcon} />
+        <SidebarMenuItem text="Notifications" Icon={BellIcon} />
+        <SidebarMenuItem text="Messages" Icon={InboxIcon} />
+        <SidebarMenuItem text="Bookmark" Icon={BookmarkIcon} />
+        <SidebarMenuItem text="Lists" Icon={ClipboardIcon} />
+        <SidebarMenuItem text="Profile" Icon={UserIcon} />
+        <SidebarMenuItem text="More" Icon={DotsCircleHorizontalIcon} />
+      </div>
+
+      {/* Button */}
+
+      <button className="bg-blue-400 text-white  rounded-full w-56 h-12 font-bold shadow-md hover:brightness-95 text-lg hidden xl:inline ">
+        Tweet
+      </button>
+
+      {/* Mini Profile */}
+      <div className="hoverEffect text-gray-700 flex items-center justify-center xl:justify-start mt-auto">
+        <img
+          src="https://newprofilepic2.photo-cdn.net//assets/images/article/profile.jpg"
+          alt="user-image"
+          className="h-10 w-10 rounded-full xl:mr-2"
+        />
+        <div className="leading-5 hidden xl:inline">
+          <h4 className="font-bold ">Lesley Connor</h4>
+          <p className="text-gray-500">@lesleybrand</p>
+        </div>
+        <DotsHorizontalIcon className="h-5 xl:ml-8 hidden xl:inline" />
+      </div>
+    </div>
+  );
+}
