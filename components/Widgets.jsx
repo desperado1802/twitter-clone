ee00e9786e7fe1d2ec1eccbc041dad3f39287d36
desperado1802 <mumun_ali@hotmail.com>
2023-03-21 12:32:28 +0200

    fixed search bar's margin

--- components/Widgets.jsx
+++ components/Widgets.jsx
@@ -3,9 +3,9 @@ import React from "react";
 
 export default function Widgets() {
   return (
-    <div className="xl:w-[600px] hidden lg:inline">
+    <div className="xl:w-[600px] hidden lg:inline ml-8 space-y-5">
       <div className="w-[90%] xl:w-[75%] sticky top-0 bg-white py-1.5 z-50">
-        <div className="flex items-center p-3 rounded-full bg-red-400 relative ml-8 space-y-5">
+        <div className="flex items-center p-3 rounded-full bg-red-400 relative ">
           <SearchIcon className="h-5 z-50 text-gray-500" />
           <input
             type="text"
