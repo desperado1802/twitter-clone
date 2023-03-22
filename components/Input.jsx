d3c2b9de1cc55716c3cc1dcdb4845d002dcbf548
desperado1802 <mumun_ali@hotmail.com>
2023-03-22 12:58:22 +0200

    created the functionality to send input and image data to firebase, and added loading while uploading

--- components/Input.jsx
+++ components/Input.jsx
@@ -1,9 +1,63 @@
+import { db, storage } from "@/firebase";
 import { PhotographIcon, EmojiHappyIcon } from "@heroicons/react/outline";
+import { XIcon } from "@heroicons/react/solid";
+import {
+  addDoc,
+  collection,
+  doc,
+  serverTimestamp,
+  updateDoc,
+} from "firebase/firestore";
+import { getDownloadURL, ref, uploadString } from "firebase/storage";
 import { useSession, signOut } from "next-auth/react";
+import { useRef, useState } from "react";
 
 export default function Input() {
   const { data: session } = useSession();
-  console.log(session);
+  const [input, setInput] = useState("");
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const filePickerRef = useRef("");
+
+  const sendPost = async () => {
+    if (loading) return;
+    setLoading(true);
+    const docRef = await addDoc(collection(db, "posts"), {
+      id: session.user.uid,
+      text: input,
+      userImg: session.user.image,
+      timestamp: serverTimestamp(),
+      name: session.user.name,
+      username: session.user.username,
+    });
+
+    const imageRef = ref(storage, `posts/${docRef.id}/image`);
+
+    if (selectedFile) {
+      await uploadString(imageRef, selectedFile, "data_url").then(async () => {
+        const downloadURL = await getDownloadURL(imageRef);
+        await updateDoc(doc(db, "posts", docRef.id), {
+          image: downloadURL,
+        });
+      });
+    }
+
+    setInput("");
+    setSelectedFile(null);
+    setLoading(false);
+  };
+
+  const addImageToPost = (e) => {
+    const reader = new FileReader();
+    if (e.target.files[0]) {
+      reader.readAsDataURL(e.target.files[0]);
+    }
+
+    reader.onload = (readerEvent) => {
+      setSelectedFile(readerEvent.target.result);
+    };
+  };
+
   return (
     <>
       {session && (
@@ -20,17 +74,49 @@ export default function Input() {
                 placeholder="What's happening"
                 rows="2"
                 className="w-full border-none focus:ring-0 text-lg placeholder-gray-700 tracking-wide min-h-[50px] text-gray-700 "
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
               ></textarea>
             </div>
-            <div className="flex items-center justify-between pt-2.5">
-              <div className="flex">
-                <PhotographIcon className="hoverEffect h-10 w-10 p-2 text-sky-500 hover:bg-sky-100 " />
-                <EmojiHappyIcon className="hoverEffect h-10 w-10 p-2 text-sky-500 hover:bg-sky-100 " />
+            {selectedFile && (
+              <div className="relative">
+                <XIcon
+                  onClick={() => setSelectedFile(null)}
+                  className="h-7 text-black absolute cursor-pointer shadow-sm shadow-white rounded-full"
+                />
+                <img
+                  src={selectedFile}
+                  alt="selected image"
+                  className={`${loading && "animate-pulse"}`}
+                />
               </div>
-              <button className="bg-blue-400 text-white px-4 py-1.5 rounded-full font-bold shadow-md hover:brightness-95 disabled:opacity-50">
-                Tweet
-              </button>
-            </div>
+            )}
+            {!loading && (
+              <div className="flex items-center justify-between pt-2.5">
+                <div className="flex">
+                  <div
+                    className=""
+                    onClick={() => filePickerRef.current.click()}
+                  >
+                    <PhotographIcon className="hoverEffect h-10 w-10 p-2 text-sky-500 hover:bg-sky-100 " />
+                    <input
+                      type="file"
+                      hidden
+                      ref={filePickerRef}
+                      onChange={addImageToPost}
+                    />
+                  </div>
+                  <EmojiHappyIcon className="hoverEffect h-10 w-10 p-2 text-sky-500 hover:bg-sky-100 " />
+                </div>
+                <button
+                  onClick={sendPost}
+                  disabled={!input.trim()}
+                  className="bg-blue-400 text-white px-4 py-1.5 rounded-full font-bold shadow-md hover:brightness-95 disabled:opacity-50"
+                >
+                  Tweet
+                </button>
+              </div>
+            )}
           </div>
         </div>
       )}
