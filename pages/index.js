ddafaae0ad92e12e36b27df71ed64fb9f3e9dd4e
desperado1802 <mumun_ali@hotmail.com>
2023-03-23 12:12:25 +0200

    installed and created state with recoil, we need it for the comment pop-up

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google";
 import Sidebar from "@/components/Sidebar";
 import Feed from "@/components/Feed";
 import Widgets from "@/components/Widgets";
+import CommentModal from "@/components/CommentModal";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -32,6 +33,7 @@ export default function Home({ newsResults, randomUsersResults }) {
         />
 
         {/* Modal */}
+        <CommentModal />
       </main>
     </>
   );
