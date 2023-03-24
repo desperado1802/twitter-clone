7135cee9007c9b60362ff7c70b8b4cf3db39ff03
desperado1802 <mumun_ali@hotmail.com>
2023-03-24 12:08:49 +0200

    fixed the secret error

--- components/Widgets.jsx
+++ components/Widgets.jsx
@@ -9,7 +9,7 @@ export default function Widgets({ newsResults, randomUsersResults }) {
   return (
     <div className="xl:w-[600px] hidden lg:inline ml-8 space-y-5">
       <div className="w-[90%] xl:w-[75%] sticky top-0 bg-white py-1.5 z-50">
-        <div className="flex items-center p-3 rounded-full bg-red-400 relative ">
+        <div className="flex items-center p-3 rounded-full relative ">
           <SearchIcon className="h-5 z-50 text-gray-500" />
           <input
             type="text"
